e1bfbee6a175366a8116a473f56fca2b95db1d57
alfanme <55023496+alfanme@users.noreply.github.com>
2020-11-02 19:14:11 +0700

    Updates

---
+++ precache-manifest.23fdf7f2df310b46d37d9a3c645e2063.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422cc35bcf5674c07a44c75dd0c7f5fe",
+    "url": "/casoot/index.html"
+  },
+  {
+    "revision": "6324250fa9e7485b8bde",
+    "url": "/casoot/static/css/main.d35e21d4.chunk.css"
+  },
+  {
+    "revision": "ed0de1ab79b7f25b7851",
+    "url": "/casoot/static/js/2.7a976137.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "/casoot/static/js/2.7a976137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6324250fa9e7485b8bde",
+    "url": "/casoot/static/js/main.0d3037c8.chunk.js"
+  },
+  {
+    "revision": "662c8be6c0d68570e5ab",
+    "url": "/casoot/static/js/runtime-main.a3310b02.js"
+  },
+  {
+    "revision": "7b886193788a0386c13243e2ac1f32f0",
+    "url": "/casoot/static/media/HarieLow.7b886193.jpg"
+  },
+  {
+    "revision": "d20d249cb72408e9ca0d31d8b7edf205",
+    "url": "/casoot/static/media/Logo.d20d249c.svg"
+  }
+]);
